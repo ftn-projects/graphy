d7cabdc0b93f3a93dd91bae59718f86435244085
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-02-04 00:20:49 +0100

    Improve plugin packaging

---
+++ API/src/graphy_api/services/__init__.py
@@ -0,0 +1,2 @@
+from .service_base import ServiceBase
+from .data_source_service import DataSourceService
--- API/src/graphy_api/services/data_source_service.py
+++ API/src/graphy_api/services/data_source_service.py
@@ -1,18 +1,18 @@
 from abc import abstractmethod
-from graphy_api.services.service_base import ServiceBase
-from graphy_api.services.source_reader.source_reader import SourceReader
+from graphy_api.services import ServiceBase
+from graphy_api.services.reader import SourceReader
 
 
 class DataSourceService(ServiceBase):
     def __init__(self):
-        self.__reader: SourceReader = None
+        self.__reader: SourceReader | None = None
 
     @property
-    def reader(self) -> SourceReader:
+    def reader(self) -> SourceReader | None:
         return self.__reader
 
     @reader.setter
-    def reader(self, reader) -> None:
+    def reader(self, reader: SourceReader) -> None:
         self.__reader = reader
 
     @abstractmethod
