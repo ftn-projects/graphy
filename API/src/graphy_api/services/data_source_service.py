777c9c50b6da93874f3126b8242435b54b86a11b
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2024-02-07 11:02:08 +0100

    Upgrade data source services

--- API/src/graphy_api/services/data_source_service.py
+++ API/src/graphy_api/services/data_source_service.py
@@ -1,28 +1,24 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABCMeta
 from graphy_api.services import ServiceBase
 from graphy_api.services.reader import SourceReader
+from graphy_api.models import Graph
 
 
-class DataSourceService(ServiceBase):
-    def __init__(self):
-        self.__reader: SourceReader | None = None
+class DataSourceService(ServiceBase, metaclass=ABCMeta):
+    def init(self):
+        self.reader: SourceReader | None = None
 
     @property
     def reader(self) -> SourceReader | None:
-        return self.__reader
+        return self.reader
 
     @reader.setter
     def reader(self, reader: SourceReader) -> None:
-        self.__reader = reader
+        self.reader = reader
+
+    def load(self) -> Graph:
+        return self._load_string(self.reader.read())
 
     @abstractmethod
-    def identifier(self):
-        pass
-
-    @abstractmethod
-    def name(self):
-        pass
-
-    @abstractmethod
-    def load(self):
+    def _load_string(self, text: str) -> Graph:
         pass
